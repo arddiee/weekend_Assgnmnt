7067fbd8e6a127fdc5f1580798796d0c00fa710b
embash <bashir.ibrahim@stetis.com>
2019-10-19 09:46:51 -0400

    new assignment submit

---
+++ src/ToolBox/Pages/Index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Style.css'
+
+class componentName extends Component {
+  render() {
+    return (
+      <div>
+            <header className= "header">
+                <div>
+                  <h1>
+                    ILLUMINARE
+                  </h1>
+                </div>
+            </header>
+      </div>
+    )
+  }
+}
+
+export default componentName
