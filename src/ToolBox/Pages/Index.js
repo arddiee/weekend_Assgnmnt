dae1353a52018bf664a36bb04f4cbaec6661ad45
embash <bashir.ibrahim@stetis.com>
2019-10-21 03:29:04 -0400

    second commit

--- src/ToolBox/Pages/Index.js
+++ src/ToolBox/Pages/Index.js
@@ -8,7 +8,17 @@ class componentName extends Component {
             <header className= "header">
                 <div>
                   <h1>
-                    ILLUMINARE
+                    {/* <div className="logo-box">
+                        <img src=".../ToolBox/illuminare_gold.png" alt ="logo" className="logo" />
+                    </div> */}
+                    <div className="text-box">
+                    <h1 className="heading-primary">
+                          <span className="heading-primary-main"> ILLUMINARE </span>
+                          <span className="heading-primary-sub">  my portfolio </span>
+                      </h1>
+                      <a href="#" className="btn btn-white"> Discover portfolio</a>
+                    </div>
+                      
                   </h1>
                 </div>
             </header>
